0959b236020a42cf62123ce9ee1d7fba2eb72647
Boyan Mihaylov <hey@boyan.io>
2018-02-07 07:30:26 +0100

    Move .htaccess out of dist and copy it when building a deployment package

--- my-post-build.js
+++ my-post-build.js
@@ -2,10 +2,21 @@ const { promisify } = require('util');
 const fs = require('fs');
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
+const copyFile = promisify(fs.copyFile);
 
-return (async () => {
+async function fixBaseHref() {
   const indexHtml = await readFile('dist/index.html', 'utf8');
   const updatedHtml = indexHtml.replace('<base href="/">', '<base href="/angular-wasm/">');
   await writeFile('dist/index.html', updatedHtml);
   console.log('base href fixed on dist/index.html');
+}
+
+async function copyHtaccess() {
+  await copyFile('src/.htaccess', 'dist/.htaccess');
+  console.log('.htaccess copied to dist');
+}
+
+return (async () => {
+  await fixBaseHref();
+  await copyHtaccess();
 })();
