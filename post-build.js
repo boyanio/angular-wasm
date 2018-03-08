535668fd2f39c566456debb1fb5332b1db6ad3a7
Boyan Mihaylov <hey@boyan.io>
2018-03-08 18:58:38 +0100

    Use `baseHref` configuration in `.angular-cli.json` rather than manipulating the output file manually

--- post-build.js
+++ post-build.js
@@ -1,16 +1,7 @@
 const { promisify } = require('util');
 const fs = require('fs');
 
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
 const copyFile = promisify(fs.copyFile);
-
-async function fixBaseHref() {
-  const indexHtml = await readFile('dist/index.html', 'utf8');
-  const updatedHtml = indexHtml.replace('<base href="/">', '<base href="/angular-wasm/">');
-  await writeFile('dist/index.html', updatedHtml);
-  console.log('base href fixed on dist/index.html');
-}
 
 async function copyFileToFolder(src, dest) {
   await copyFile(src, dest);
@@ -18,6 +9,5 @@ async function copyFileToFolder(src, dest) {
 }
 
 return (async () => {
-  await fixBaseHref();
   await copyFileToFolder('src/.htaccess', 'dist/.htaccess');
 })();
