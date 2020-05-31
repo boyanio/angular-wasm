6352026b7ad4688c73c3e288c7bc824ae6ab94bd
Boyan Mihaylov <hey@boyan.io>
2020-05-31 14:47:05 +0300

    Emscripten module function returns a promise

--- src/app/wasm/emscripten-wasm.component.ts
+++ src/app/wasm/emscripten-wasm.component.ts
@@ -4,6 +4,8 @@ import { environment } from "../../environments/environment";
 
 type EmscriptenModuleDecorator = (module: EmscriptenModule) => void;
 
+const noopModuleDecorator = ((mod: EmscriptenModule) => mod);
+
 export abstract class EmscriptenWasmComponent implements AfterViewInit {
   private resolvedModule: EmscriptenModule;
   protected moduleDecorator: EmscriptenModuleDecorator;
@@ -23,10 +25,12 @@ export abstract class EmscriptenWasmComponent implements AfterViewInit {
       const module = <EmscriptenModule>{
         locateFile: (file: string) => `${environment.wasmAssetsPath}/${file}`,
       };
-      const moduleDecorator: EmscriptenModuleDecorator = this.moduleDecorator || ((mod: EmscriptenModule) => mod);
+      const moduleDecorator: EmscriptenModuleDecorator = this.moduleDecorator || noopModuleDecorator;
       moduleDecorator(module);
 
-      this.resolvedModule = window[this.moduleExportName](module);
+      return window[this.moduleExportName](module);
+    }).then(mod => {
+      this.resolvedModule = mod;
     });
   }
 }
