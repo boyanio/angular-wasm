b9077620e51c6e74ded89b11cfbfef2421a5f48c
Boyan Mihaylov <hey@boyan.io>
2020-05-31 17:00:18 +0300

    Simplify compile-wasm.js

--- scripts/compile-wasm.js
+++ scripts/compile-wasm.js
@@ -1,66 +1,43 @@
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
+const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const chalk = require("chalk");
 
 const rootDir = path.resolve(__dirname, "../");
 
-const pfs = ["exists", "mkdir", "readdir", "unlink", "lstat"].reduce(
-  (result, func) => Object.assign(result, { [func]: promisify(fs[func]) }),
-  {}
-);
-
-const ensureWasmOutputDirExists = async () => {
+const ensureWasmOutputDirExists = () => {
   console.log("Preparing wasm output folder...\n");
 
   const src = path.resolve(rootDir, "src/assets/wasm");
-  if (await pfs.exists(src)) {
-    const files = await pfs.readdir(src);
+  if (fs.existsSync(src)) {
+    const files = fs.readdirSync(src);
     for (let file of files) {
-      await pfs.unlink(path.join(src, file));
+      fs.unlinkSync(path.join(src, file));
     }
   } else {
-    await pfs.mkdir(src);
+    fs.mkdirSync(src);
   }
 };
 
-const execCommand = (command) =>
-  exec(command, { cwd: rootDir }).then(({ stdout, stderr }) => {
-    const status = stdout + stderr;
-    if (status && status !== "undefined") {
-      console.log(chalk.grey(status));
-    }
-  });
-
-const compileWasmSources = async () => {
-  console.log("Compiling wasm sources...\n");
+const compileWasmSources = () => {
+  console.log("Compiling wasm sources...");
 
   const wasmDir = path.resolve(rootDir, "src/app/wasm");
-  for (let item of await pfs.readdir(wasmDir)) {
+  for (let item of fs.readdirSync(wasmDir)) {
     const itemPath = path.join(wasmDir, item);
-    if ((await pfs.lstat(itemPath)).isDirectory()) {
-      const buildFilePath = path.join(itemPath, "build-cmd.js");
-      const { cmd } = require(buildFilePath);
-
-      console.log("Compiling wasm source for", chalk.green(item));
-      console.log(`${cmd}\n`);
-      try {
-        await execCommand(cmd);
-      } catch ({ stderr }) {
-        console.error(chalk.red(`Error: ${stderr}\n`));
-        process.exit(-1);
-      }
+    if (!fs.lstatSync(itemPath).isDirectory()) {
+      continue;
     }
+
+    const buildFilePath = path.join(itemPath, "build-cmd.js");
+    const { cmd } = require(buildFilePath);
+
+    console.log("\nCompiling wasm source for", chalk.green(item));
+    console.log(cmd);
+    execSync(cmd, { cwd: rootDir, stdio: "inherit" });
   }
 };
 
-const allPromises = [ensureWasmOutputDirExists, compileWasmSources];
-
-return (async () => {
-  for (const promise of allPromises) {
-    await promise();
-  }
-
-  console.log("All sources have been successfully compiled.\n");
-})();
+ensureWasmOutputDirExists();
+compileWasmSources();
+console.log("\nAll sources have been successfully compiled.");
