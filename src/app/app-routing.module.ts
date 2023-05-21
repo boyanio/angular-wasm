00434c4b1cac5a022943a65e7d578e0d3036ecce
Boyan Mihaylov <boyan.mihaylov@gtmhub.com>
2023-05-21 10:12:12 +0300

    Upgrade to Angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
